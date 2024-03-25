16402e87504c890368884a94de89fe59bb449f5f
baptt123 <21130529@st.hcmuaf.edu.vn>
2024-03-25 18:54:27 +0700

    up tiếp các file nữa

---
+++ TestFragment/app/src/main/java/com/example/testfragment/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.testfragment;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Fragment_1 fragment1=new Fragment_1();
+//        Fragment_2 fragment2=new Fragment_2();
+//        FragmentManager fragmentManager=getFragmentManager();
+//        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+//        fragmentTransaction.add(R.id.....,fragment1);
+//        fragmentTransaction.add(R.id....,fragment2);
+//        fragmentTransaction.commit();
+
+    }
+}
